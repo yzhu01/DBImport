d282136b94fbe739c962ff52935fc70b5379235d
William Chen <kis87988@gmail.com>
2019-09-04 10:47:46 -0700

    feat: add pydocstring package for docstring checking

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -7,6 +7,26 @@ set -e
   # http://stackoverflow.com/a/21334985/1123955
   exit 0
 }
-
-autopep8 --verbose --in-place --recursive .
+GIT_ROOT=$(git rev-parse --show-toplevel)
+echo "start autopep8 for code style formating"
+autopep8 --verbose --in-place --recursive "${GIT_ROOT}"
+echo "start pydocstyle for code docstring checking"
+python -m pydocstyle "${GIT_ROOT}"/DBImport
+  cat <<'_STR_'
+       _ _                                   _ _    
+      (_) |                                 (_) |   
+  __ _ _| |_    ___ ___  _ __ ___  _ __ ___  _| |_  
+ / _` | | __|  / __/ _ \| '_ ` _ \| '_ ` _ \| | __| 
+| (_| | | |_  | (_| (_) | | | | | | | | | | | | |_  
+ \__, |_|\__|  \___\___/|_| |_| |_|_| |_| |_|_|\__| 
+  __/ |                                             
+ |___/                                              
+                                 _ _                
+                                | | |               
+ ___ _   _  ___ ___ ___  ___  __| | |               
+/ __| | | |/ __/ __/ _ \/ _ \/ _` | |               
+\__ \ |_| | (_| (_|  __/  __/ (_| |_|               
+|___/\__,_|\___\___\___|\___|\__,_(_)               
+                                                                                                      
+_STR_
 
