46775e2f546a37071825a1aac5148e6f6b451609
zhirezhixin <yifeil@berkeley.edu>
2019-12-01 21:07:39 -0800

    fix: fix test01

--- tests/test01_config_existance.py
+++
@@ -1,9 +0,0 @@
-
-
-def test_file_path_config():
-	with open('../config/file_paths.config', 'r') as f:
-		assert f != NULL
-
-def test_setting_config():
-	with open('../config/setting.config', 'r') as f:
-		assert f != NULL
