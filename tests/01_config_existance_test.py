152fa9bf94ef1a4ef9dce8577905a21dbff30000
yifeili98 <yifeil@berkeley.edu>
2020-01-25 21:37:32 -0800

    fix: remove config existance test

--- tests/01_config_existance_test.py
+++
@@ -1,6 +0,0 @@
-import os
-def test_file_path_config():
-	assert os.path.exists(os.getcwd() + "/config/file_paths.config")
-
-def test_setting_config():
-	assert os.path.exists(os.getcwd() + "/config/setting.config")
