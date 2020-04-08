b08e049af3f58d4f74b4429a19e1de797f13730b
yzhu <yzhu@ucla.edu>
2020-04-07 22:00:23 -0700

    style: fixed some small details in codes

--- src/InsertData.py
+++ src/InsertData.py
@@ -2,7 +2,7 @@
 """Insert Data component.
 
 This module gets lists of Course and Department objects from ReadCourseData.py
-and insert those data into desired databse
+and inserts those data into the desired database
 """
 
 
@@ -17,10 +17,10 @@ from pymongo import MongoClient
 from pymongo import errors as mongoerrors
 from pathlib import Path
 
-logging.basicConfig(filename = '../log/' + 
-                str(datetime.datetime.now()).replace(' ', '_').replace(':', '')[:17] + '.log', 
-                level=logging.INFO, 
-                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='../log/' +
+                    str(datetime.datetime.now()).replace(' ', '_').replace(':', '')[:17] + '.log',
+                    level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 env_config = ConfigParser()
 env_config.read(Path('..') / 'config' / 'setting.config')
@@ -28,7 +28,7 @@ mongo_config = env_config['MongoDB']
 QUARTER_INDEX = -16
 
 def get_db():
-    """Get MongoDB username and password from config file and returns desired databse.
+    """Get MongoDB username and password from the config file and return the desired database.
 
     Raises:
         pymongo.errors: possibly connection errors or conficuration errors
@@ -44,7 +44,7 @@ def get_db():
 
 
 def check_file_open(filename):
-    """Check if the json file exist in the specific directory.
+    """Check if the json file exists in the specified directory.
 
     Args:
         filename: the path to the desired file
@@ -55,30 +55,31 @@ def check_file_open(filename):
 
     """
     if filename:
-        with open(filename, 'r') as f:
-            return json.load(f)
+        with open(filename, 'r') as file:
+            return json.load(file)
     raise FileNotFoundError('File', filename, 'is not found!')
 
 
 def insert_data(course_list, dept_list, quarter_name):
-    """Insert data into databse.
+    """Insert data into database.
 
-    Get every single Course and Department object from the lists and 
-    insert them into the correct database collections 
+    Get every single Course and Department object from the lists and
+    insert them into the correct database collections
         (named quarter_name + 'course'/'departments')
 
     Args:
         course_list: the list of Course objects
         dept_list: the list of Department objects
+        quarter_name: str, the name of the quarter
     Raises:
-        pymongo.errors: possibly connection errors or conficuration errors
+        pymongo.errors: possibly connection errors or configuration errors
     Returns:
         None
- 
+
     """
-    db = get_db()
-    course_collection = db[quarter_name + ' courses']
-    dept_collection = db[quarter_name + ' departments']
+    database = get_db()
+    course_collection = database[quarter_name + ' courses']
+    dept_collection = database[quarter_name + ' departments']
 
     for course in course_list:
         temp_course = MessageToDict(course)
@@ -91,10 +92,10 @@ def insert_data(course_list, dept_list, quarter_name):
 def main():
     """Runner of this DBImport program.
 
-    With help of other functions, this main function could read the data from
-    json files and put them into desired databses.
+    With the help of other functions, this main function could read the data from
+    JSON files and put them into desired databases.
     """
-    logger.info('Excecution Started.')
+    logger.info('InsertData.py Excecution Started.')
     config = ConfigParser()
     config.read(Path('..') / 'config' / env_config['Config']['Config_File_Name'])
     path = config['locations']['path']
@@ -104,16 +105,18 @@ def main():
         while config['locations'][str(year)]:
             all_quarters_in_year = config['locations'][str(year)].split(',')
             for each_quarter in all_quarters_in_year:
-                quarter_name, filename = each_quarter[:QUARTER_INDEX].replace('_', ' '), path + each_quarter
+                quarter_name = each_quarter[:QUARTER_INDEX].replace('_', ' ')
+                filename = path + each_quarter
                 course_raw_data = check_file_open(filename)
                 course_list, department_list = from_raw_to_list(course_raw_data, quarter_name)
                 insert_data(course_list, department_list, quarter_name)
             year += 1
     except mongoerrors.PyMongoError:
         logger.error('Error with MongoDB!')
-    except (FileNotFoundError,KeyError) as e:
-        logger.error(str(e))
-    logger.info('Excecution Finished.')
+    except (FileNotFoundError, KeyError) as err:
+        logger.error(str(err))
+    logger.info('InsertData.py Excecution Finished.')
+
 
 if __name__ == "__main__":
     main()
